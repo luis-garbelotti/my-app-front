b4d459d41edc350fa852e4b487976a296d1962cb
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-05-17 23:40:58 -0300

    feat: navbar

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, useState } from 'react';
+
+const AuthContext = createContext<any | undefined>(undefined);
+
+export function AuthProvider({ children }: any) {
+  const persistedAuth = JSON.parse(localStorage.getItem('auth') as string);
+  const [auth, setAuth] = useState(persistedAuth);
+
+  function login(authData: any) {
+    setAuth(authData);
+    localStorage.setItem('auth', JSON.stringify(authData));
+  }
+
+  function logout(){
+    setAuth(null);
+    localStorage.removeItem(auth);
+  }
+
+  return (
+    <AuthContext.Provider value={{ auth, login, setAuth, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContext;
