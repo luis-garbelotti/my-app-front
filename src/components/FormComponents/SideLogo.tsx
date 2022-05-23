3a28a52149160a7c65e38ada6594a1b60174b50e
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-05-23 19:25:12 -0300

    chore: change side logo

--- src/components/FormComponents/SideLogo.tsx
+++ src/components/FormComponents/SideLogo.tsx
@@ -2,21 +2,29 @@ import { Box } from '@mui/material';
 import Logo from '../Logo/Logo';
 
 const style = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  form: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    color: '#fff',
 
-  width: '140%',
-  height: '100vh',
-  marginLeft: '-30px',
-  backgroundColor: '#BF0000',
-  borderRadius: '0 15px 15px 0',
-};
+    width: '140%',
+    height: '100vh',
+    marginLeft: '-30px',
+    backgroundColor: '#BF0000',
+    borderRadius: '0 15px 15px 0',
+  },
+  text:{
+    fontStyle: 'italic'
+  }
+  };
 
 function SideLogo() {
   return (
-    <Box component="form" sx={style}>
+    <Box component="form" sx={style.form}>
       <Logo/>
+      <Box component='h3' sx={style.text}>Controle seus projetos freelancer em um só lugar!</Box>
     </Box>
   );
 }
